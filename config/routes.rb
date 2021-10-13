d1deba5d5730919b7fc78fa67a6e5cfd6fd92eef
EC2 Default User <ec2-user@ip-172-31-7-220.ec2.internal>
2021-10-13 15:16:23 +0000

    [update]ジャンル一覧・追加画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get '/' => 'homes#top'
     devise_for :admins, path: "", controllers: {
       sessions: "admin/admins/sessions"
     }
-    get '/' => 'homes#top'
+    resources :genres, only: [:index, :create, :edit, :update]
   end
 end
