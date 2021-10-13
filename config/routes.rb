1e1a11e62f613cb9af89feaba908edb1d28693fd
EC2 Default User <ec2-user@ip-172-31-7-220.ec2.internal>
2021-10-13 14:11:15 +0000

    [update]管理者ログイン画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
-    devise_for :admins, path: ""
+    devise_for :admins, path: "", controllers: {
+      sessions: "admin/admins/sessions"
+    }
+    get '/' => 'homes#top'
   end
 end
