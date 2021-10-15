ec0bb4f369a8963f72563cbdf7766545b08c623f
EC2 Default User <ec2-user@ip-172-31-7-220.ec2.internal>
2021-10-15 20:45:56 +0000

    [Add]Itemモデルにvalidationを追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,19 +9,33 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_params)
-    item.save
-    redirect_to "/admin/items"
+    @item = Item.new(item_params)
+    @genres = Genre.all
+    if @item.save
+      redirect_to "/admin/items/#{@item.id}"
+    else
+      render :new
+    end
   end
 
   def show
-    @item = Item.find(params[:id])
+    @item = Item.joins(:genre).find(params[:id])
+    @tax = 1.1
   end
 
   def edit
+    @item = Item.joins(:genre).find(params[:id])
+    @genres = Genre.all
   end
 
   def update
+    @item = Item.joins(:genre).find(params[:id])
+    @genres = Genre.all
+    if @item.update(item_params)
+      redirect_to "/admin/items/#{@item.id}"
+    else
+      render :edit
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :genre, optional: true
   attachment :image
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  validates :is_active, presence: true
+  validates :genre_id, presence: true
 end
