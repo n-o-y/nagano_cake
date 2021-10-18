3c823bd56be8123949a268bd8894acedc33c8201
EC2 Default User <ec2-user@ip-172-31-7-220.ec2.internal>
2021-10-18 12:37:51 +0000

    [Add]genresテーブルのcreated_atカラムとupdated_atカラムにdefault: -> { now }を追加

--- db/migrate/20211018121907_create_genres.rb
+++ db/migrate/20211018121907_create_genres.rb
@@ -2,10 +2,8 @@ class CreateGenres < ActiveRecord::Migration[5.2]
   def change
     create_table :genres do |t|
       t.string :name
-      t.datetime :created_at
-      t.datetime :updated_at
-
-      t.timestamps
+      t.datetime :created_at, default: -> { "now" }
+      t.datetime :updated_at, default: -> { "now" }
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_16_085654) do
+ActiveRecord::Schema.define(version: 2021_10_18_121907) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,9 +40,9 @@ ActiveRecord::Schema.define(version: 2021_10_16_085654) do
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "items", force: :cascade do |t|
