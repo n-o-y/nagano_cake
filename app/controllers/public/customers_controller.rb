912a9804f3cab8ace2b880ef170159f8b6c65533
EC2 Default User <ec2-user@ip-172-31-44-191.ec2.internal>
2021-10-23 06:25:02 +0000

    [Add]会員退会機能を作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,19 +1,10 @@
 class Public::CustomersController < ApplicationController
-  def show
-    @customer = Customer.find(1)
-  end
-
-  def edit
-    @customer = current_customer
-    redirect_to "/customers/my_page"
-  end
-
-  def update
-  end
-
   def unsubscribe
   end
 
   def withdraw
+    customer = current_customer
+    customer.update(is_active: false)
+    redirect_to "/"
   end
 end
