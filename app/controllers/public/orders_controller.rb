005d5270dbf32ee0e7fb2a8231f5d60e60a6553e
EC2 Default User <ec2-user@ip-172-31-44-191.ec2.internal>
2021-10-28 13:20:09 +0000

    [Add]注文情報入力画面を作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,9 +1,26 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
+    @addresses = Address.where(customer_id: current_customer.id)
   end
 
   def confirm
+    @order = Order.new
+    @order.payment_method = params[:order][:payment_method]
+    if params[:order][:address_option] == "1"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:address_option] == "2"
+      @order.postal_code = Address.find(params[:order][:address_id]).postal_code
+      @order.address = Address.find(params[:order][:address_id]).address
+      @order.name = Address.find(params[:order][:address_id]).name
+    elsif params[:order][:address_option] == "3"
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+    end
+
   end
 
   def complete
@@ -17,4 +34,10 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+  private
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :payment_method)
+  end
+
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :customer, optional: true
+  def full_address
+    "〒" + postal_code + address + name
+  end
 end
